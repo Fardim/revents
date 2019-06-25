d6d2791587d84d0843c485a53a829db88935b076
FARDIM KAISER <fardim_drmc_9094@yahoo.com>
2019-06-26 02:32:14 +0600

    Mod 3 complete

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,12 +3,16 @@ import EventListItem from './EventListItem';
 class EventList extends Component {
     state = {};
     render() {
+        const { events } = this.props;
         return (
             <div>
                 <h1>EventList</h1>
+                {events.map(event => (
+                    <EventListItem key={event.id} event={event} />
+                ))}
+                {/* 
                 <EventListItem />
-                <EventListItem />
-                <EventListItem />
+                <EventListItem /> */}
             </div>
         );
     }
